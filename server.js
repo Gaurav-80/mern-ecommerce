856435dc6691bdbdf03eab284e12c4504e1bc2f4
Gaurav <95282351+Gaurav-80@users.noreply.github.com>
2023-03-24 04:53:05 -0600

    updated

--- server.js
+++ server.js
@@ -8,11 +8,16 @@ import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import cors from "cors";
 import path from "path";
+import {fileURLToPath} from 'url';
 //configure env
 dotenv.config();
 
 //Database Connection
 connectDB();
+
+//esmodule fix
+const __filename= fileURLToPath(import.meta.url);
+const __dirname= path.dirname(__filename)
 
 const app = express();
 
